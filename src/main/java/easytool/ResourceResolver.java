c75023aa4c37f7b6814d364896b25308c098279c
egorklepikov <egor.klepickov95@yandex.ru>
2020-06-13 13:43:51 +0300

    Unified ResourceResolver

--- src/main/java/easytool/ResourceResolver.java
+++ src/main/java/easytool/ResourceResolver.java
@@ -7,45 +7,8 @@ public class ResourceResolver {
         StringBuilder resultCommand = new StringBuilder();
         String[] attrs = command.split(" ");
         ClassLoader classLoader = ResourceResolver.class.getClassLoader();
-        switch (attrs[0]) {
-            case "adb" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("adb")).getPath());
-                break;
-            }
-            case "apktool.bat" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("apktool.bat")).getPath());
-                break;
-            }
-            case "apktool.jar" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("apktool.jar")).getPath());
-                break;
-            }
-            case "apktool-linux" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("apktool-linux")).getPath());
-                break;
-            }
-            case "apktool-osx" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("apktool-osx")).getPath());
-                break;
-            }
-            case "debug.keystore" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("debug.keystore")).getPath());
-                break;
-            }
-            case "dx" : {
-                resultCommand.append(Objects.requireNonNull(classLoader.getResource("dx")).getPath());
-                break;
-            }
-            case "jarsigner" : {
-                //TODO something is wrong here
-                resultCommand.append("jarsigner");
-                break;
-            }
-            default: {
-                resultCommand.append("unknown resource");
-            }
-        }
 
+        resultCommand.append(Objects.requireNonNull(classLoader.getResource(attrs[0])).getPath());
         resultCommand.append(" ");
         for (int index = 0; index < attrs.length; index++) {
             if (index == 0) {
