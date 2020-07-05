4a6c2c3e6a29ce99f89f3a5b7376c7477391e887
egor.klepikov <Egor.Klepikov@jetbrains.com>
2020-07-05 14:10:28 +0300

    Code refactoring

--- src/main/java/decompiletool/network/GetApkProcess.java
+++ src/main/java/decompiletool/network/GetApkProcess.java
@@ -3,33 +3,33 @@ package decompiletool.network;
 import decompiletool.cmd.Command;
 
 public class GetApkProcess implements Command {
-    public GetApkProcess(AppInformation selectedApp, AppInformation.AppRelease selectedRelease) {
+  public GetApkProcess(AppInformation selectedApp, AppInformation.AppRelease selectedRelease) {
 
-    }
+  }
 
-    @Override
-    public boolean execute() {
-        return false;
-    }
+  @Override
+  public boolean execute() {
+    return false;
+  }
 
-    @Override
-    public void startNotification() {
-        System.out.println("----------------------------------------------------");
-        System.out.println("Loading APK...");
-        System.out.println("----------------------------------------------------");
-    }
+  @Override
+  public void startNotification() {
+    System.out.println("----------------------------------------------------");
+    System.out.println("Loading APK...");
+    System.out.println("----------------------------------------------------");
+  }
 
-    @Override
-    public void endNotification() {
-        System.out.println("----------------------------------------------------");
-        System.out.println("Loading APK process is finished.");
-        System.out.println("----------------------------------------------------");
-    }
+  @Override
+  public void endNotification() {
+    System.out.println("----------------------------------------------------");
+    System.out.println("Loading APK process is finished.");
+    System.out.println("----------------------------------------------------");
+  }
 
-    @Override
-    public void errorNotification() {
-        System.out.println("----------------------------------------------------");
-        System.out.println("The error occurred when loading the APK.");
-        System.out.println("----------------------------------------------------");
-    }
+  @Override
+  public void errorNotification() {
+    System.out.println("----------------------------------------------------");
+    System.out.println("The error occurred when loading the APK.");
+    System.out.println("----------------------------------------------------");
+  }
 }
